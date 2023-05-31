edb91576e058be55df4892f7a6c38714d57a9c8c
bleasey <calvin7ferns@gmail.com>
2023-05-31 20:19:13 +0530

    added cors

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,11 @@ const cors = require('cors');
 const {errorMiddleware} = require('./middleware/errorMiddleware')
 
 
+const corsOptions = {
+  origin: "https://exphub.onrender.com/", // frontend URI (ReactJS)
+}
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan('tiny'))
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
