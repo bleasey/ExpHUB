a1533e91999984e01b76d55b2a43eb549a954be9
bleasey <calvin7ferns@gmail.com>
2023-05-31 21:51:16 +0530

    removed cors

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,11 @@ const cors = require('cors');
 const {errorMiddleware} = require('./middleware/errorMiddleware')
 
 
-const corsOptions = {
-  origin: "https://exphub.onrender.com/", // frontend URI (ReactJS)
-}
+// const corsOptions = {
+//   origin: "https://exphub.onrender.com/", // frontend URI (ReactJS)
+// }
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(morgan('tiny'))
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
